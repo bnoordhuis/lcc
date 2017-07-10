338b7ba93308bc8f3c093f11935157331499f345
Ben Noordhuis <info@bnoordhuis.nl>
2017-07-10 23:17:57 +0200

    Make Linux driver great again.
    * Search for libraries and dynamic linker in /usr/lib32.
    * Don't link in crtbegin.o and crtend.o, files don't exist.
    * Remove custom assert.h, not needed.
    * Add custom stddef.h.

--- etc/linux.c
+++ etc/linux.c
@@ -10,43 +10,39 @@ static char rcsid[] = "$Id$";
 
 char *suffixes[] = { ".c", ".i", ".s", ".o", ".out", 0 };
 char inputs[256] = "";
-char *cpp[] = { LCCDIR "gcc/cpp",
+char *cpp[] = { LCCDIR "lib/lcc/cpp",
 	"-U__GNUC__", "-D_POSIX_SOURCE", "-D__STDC__=1", "-D__STRICT_ANSI__",
 	"-Dunix", "-Di386", "-Dlinux",
 	"-D__unix__", "-D__i386__", "-D__linux__", "-D__signed__=signed",
 	"$1", "$2", "$3", 0 };
-char *include[] = {"-I" LCCDIR "include", "-I" LCCDIR "gcc/include", "-I/usr/include", 0 };
-char *com[] = {LCCDIR "rcc", "-target=x86/linux", "$1", "$2", "$3", 0 };
-char *as[] = { "/usr/bin/as", "-o", "$3", "$1", "$2", 0 };
+char *include[] = {
+	/*  0 */ "-I" LCCDIR "include/x86/linux",
+	/*  1 */ "-I/usr/include",
+	0 };
+char *com[] = {LCCDIR "lib/lcc/rcc", "-target=x86/linux", "$1", "$2", "$3", 0 };
+char *as[] = { "/usr/bin/as", "--32", "-o", "$3", "$1", "$2", 0 };
 char *ld[] = {
 	/*  0 */ "/usr/bin/ld", "-m", "elf_i386", "-dynamic-linker",
-	/*  4 */ "/lib/ld-linux.so.2", "-o", "$3",
-	/*  7 */ "/usr/lib/crt1.o", "/usr/lib/crti.o",
-	/*  9 */ LCCDIR "/gcc/crtbegin.o", 
-                 "$1", "$2",
-	/* 12 */ "-L" LCCDIR,
-	/* 13 */ "-llcc",
-	/* 14 */ "-L" LCCDIR "/gcc", "-lgcc", "-lc", "-lm",
-	/* 18 */ "",
-	/* 19 */ LCCDIR "/gcc/crtend.o", "/usr/lib/crtn.o",
+	/*  4 */ "/lib32/ld-linux.so.2", "-o", "$3",
+	/*  7 */ "/usr/lib32/crt1.o", "/usr/lib32/crti.o", "$1", "$2",
+	/* 11 */ "-L" LCCDIR "lib/lcc",
+	/* 12 */ "-lc", "-lm",
+	/* 14 */ "",
+	/* 15 */ "/usr/lib32/crtn.o",
 	0 };
 
 extern char *concat(char *, char *);
 
 int option(char *arg) {
   	if (strncmp(arg, "-lccdir=", 8) == 0) {
-		if (strcmp(cpp[0], LCCDIR "gcc/cpp") == 0)
-			cpp[0] = concat(&arg[8], "/gcc/cpp");
-		include[0] = concat("-I", concat(&arg[8], "/include"));
-		include[1] = concat("-I", concat(&arg[8], "/gcc/include"));
-		ld[9]  = concat(&arg[8], "/gcc/crtbegin.o");
-		ld[12] = concat("-L", &arg[8]);
-		ld[14] = concat("-L", concat(&arg[8], "/gcc"));
-		ld[19] = concat(&arg[8], "/gcc/crtend.o");
-		com[0] = concat(&arg[8], "/rcc");
+		if (strcmp(cpp[0], LCCDIR "lib/lcc/cpp") == 0)
+			cpp[0] = concat(&arg[8], "/lib/lcc/cpp");
+		include[0] = concat("-I", concat(&arg[8], "/include/x86/linux"));
+		ld[11] = concat("-L", concat(&arg[8], "/lib/lcc"));
+		com[0] = concat(&arg[8], "/lib/lcc/rcc");
 	} else if (strcmp(arg, "-p") == 0 || strcmp(arg, "-pg") == 0) {
-		ld[7] = "/usr/lib/gcrt1.o";
-		ld[18] = "-lgmon";
+		ld[7] = "/usr/lib32/gcrt1.o";
+		ld[14] = "-lgmon";
 	} else if (strcmp(arg, "-b") == 0) 
 		;
 	else if (strcmp(arg, "-g") == 0)
